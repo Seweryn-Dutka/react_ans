4dd763d691cd9320ea6b5e2907a0f294e83026a1
Seweryn <sewerynaleksdutka@gmail.com>
2023-05-23 10:03:46 +0200

    lab9

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import Posts from './components/posts';
+import Home from './components/home';
+import NotFound from './components/notFound';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="posts" element={<Posts />} />
+          <Route
+            path="*"
+            element={
+              <NotFound />
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
